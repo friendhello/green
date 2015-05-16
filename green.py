1afdc5e5a48a3dbd9db0b07b1dee263eca6d0abe
lixiaoxu <lixiaoxu10@163.com>
2015-05-16 14:43:52 +0800

    test

--- green.py
+++ green.py
@@ -1,8 +1,19 @@
-import datetime
+import datetime,random,time
 import os
 
 # from heavy import special_commit
 
+def strTimeProp(start, end, prop, frmt):
+    stime = time.mktime(time.strptime(start, frmt))
+    etime = time.mktime(time.strptime(end, frmt))
+    ptime = stime + prop * (etime - stime)
+    return int(ptime)
+
+def randomDate(start, end, frmt='%Y-%m-%d'):
+    return time.strftime(frmt, time.localtime(strTimeProp(start, end, random.random(), frmt)))
+
+def randomDateList(start, end, n, frmt='%Y-%m-%d %H:%M:%S'):
+    return randomDate(start, end, frmt)
 
 def modify():
     file = open('D:\GIthubWorkspace\Love-master\zero.md', 'r')
@@ -21,8 +32,12 @@ def commit():
 
 
 def set_sys_time(year, month, day):
-    print(year)
-    print('date %04d/%02d/%02d' % (year, month, day))
+    start = '2015-05-02 12:12:12'
+    end = '2021-11-19 00:00:00'
+    lenth = 10
+    d = randomDateList(start, end, lenth).split(' ')[0].split('-')
+    print(d)
+    print('date %04d/%02d/%02d' % (int(d[0]), int(d[1]), int(d[2])))
     os.system('date %04d/%02d/%02d' % (year, month, day))
 
 
